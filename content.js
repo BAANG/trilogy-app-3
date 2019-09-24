1f8f2e2a0f5566691eaaf50920d37b91097d1b8f
BAANG <chcelestino@gmail.com>
2019-09-24 11:05:29 -0700

    Add function to parse just url domain name

--- content.js
+++ content.js
@@ -1,5 +1,24 @@
 var firstHref = $("a[href^='http']").eq(0).attr("href");
 
+function extractHostname(url) {
+    var hostname;
+    //find & remove protocol (http, ftp, etc.) and get hostname
+
+    if (url.indexOf("//") > -1) {
+        hostname = url.split('/')[2];
+    }
+    else {
+        hostname = url.split('/')[0];
+    }
+
+    //find & remove port number
+    hostname = hostname.split(':')[0];
+    //find & remove "?"
+    hostname = hostname.split('?')[0];
+
+    return hostname;
+}
+
 // Wait for message to be received from background.js
 chrome.runtime.onMessage.addListener(
     function (request, sender, sendResponse) {
@@ -8,7 +27,7 @@ chrome.runtime.onMessage.addListener(
                 console.log(firstHref);
                 break;
             case "query_tabs":
-                console.log(firstHref, request.time)
+                console.log(extractHostname(firstHref), request.time)
         }
     }
 );
